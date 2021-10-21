4a77a55b2de9f48c8d601ee01cae860cd16f1607
nick7 <nick7@uni-bremen.de>
2021-10-21 17:13:52 +0200

    abstract domain class as starting point

--- src/torchphysics/problem/domain/domain.py
+++ src/torchphysics/problem/domain/domain.py
@@ -4,63 +4,24 @@ import warnings
 
 
 class Domain:
-    def __init__(self, space, dim=None, tol=1e-06):
+    def __init__(self, space, dim=None):
         self.space = space
-        self.tol = tol
         if dim is None:
             self.dim = self.space.dim
         else:
             self.dim = dim
 
+    @abc.abstractmethod
     @property
-    def is_initialized(self):
+    def boundary(self):
+        # Domain object of the boundary
         raise NotImplementedError
 
-    def divide_points_to_space_variables(self, points):
-        """Divides sample points of the form np.array(number_of_points, self.dim)
-        to each variable of the given Space.
-
-        Parameters
-        ----------
-        points: list, array
-            The created sample/data points, need to fit the given dimension
-        
-        Returns
-        -------
-        dict
-            A dictionary containing the input points but split up, to each 
-            variable. E.g Space = R1('x')*R1('y') then the output would be
-            output = {'x': points[:, 0:1], 'y': points[:, 1:2]}
-        """
-        output = {}
-        current_dim = 0
-        for vname in self.space:
-            v_dim = self.space[vname]
-            output[vname] = points[:, current_dim:current_dim+v_dim].astype(np.float32)
-            current_dim += v_dim
-        return output
-
-    def return_space_variables_to_point_list(self, point_dic):
-        """Concatenates sample points from a dict back to the form 
-        np.array(number_of_points, self.dim)
-
-        Parameters
-        ----------
-        point_dic: dic
-            The dictionary of points 
-            (most likely created with divide_points_to_space_variables)
-        
-        Returns
-        -------
-        points: array
-            the point array of the form np.array(number_of_points, self.dim)
-        """
-        # if the points are not a dictonary just return
-        # (not created with our sampling)
-        if isinstance(point_dic, (list, np.ndarray)):
-            return point_dic
-        point_list = list(point_dic.values())
-        return np.column_stack(point_list)
+    @abc.abstractmethod
+    @property
+    def inner(self):
+        # open domain
+        raise NotImplementedError
 
     @abc.abstractmethod
     def __add__(self, other):
@@ -99,7 +60,7 @@ class Domain:
         raise NotImplementedError
 
     @abc.abstractmethod
-    def is_inside(self, points):
+    def __contains__(self, points):
         """Checks for every point in points if it lays inside the domain.
 
         Parameters
@@ -130,7 +91,7 @@ class Domain:
         raise NotImplementedError
 
     @abc.abstractmethod
-    def sample_grid(self, n):
+    def sample_grid(self, n=None, d=None):
         """Greates a equdistant grid in the domain.
 
         Parameters
@@ -141,7 +102,7 @@ class Domain:
         raise NotImplementedError
 
     @abc.abstractmethod
-    def sample_random_uniform(self, n):
+    def sample_random_uniform(self, n=None, d=None):
         """Greates a random uniform points in the domain.
 
         Parameters
@@ -151,22 +112,55 @@ class Domain:
         """
         raise NotImplementedError
 
-    @abc.abstractmethod
-    def project_on_subspace(self, subspace):
-        raise NotImplementedError
 
     def __mul__(self, other):
         return ProductDomain(self, other)
 
-    @property
-    def boundary(self):
-        # Domain object of the boundary
-        raise NotImplementedError
+    def _divide_points_to_space_variables(self, points):
+        """Divides sample points of the form np.array(number_of_points, self.dim)
+        to each variable of the given Space.
 
-    @property
-    def inner(self):
-        # open domain
-        raise NotImplementedError
+        Parameters
+        ----------
+        points: list, array
+            The created sample/data points, need to fit the given dimension
+        
+        Returns
+        -------
+        dict
+            A dictionary containing the input points but split up, to each 
+            variable. E.g Space = R1('x')*R1('y') then the output would be
+            output = {'x': points[:, 0:1], 'y': points[:, 1:2]}
+        """
+        output = {}
+        current_dim = 0
+        for vname in self.space:
+            v_dim = self.space[vname]
+            output[vname] = points[:, current_dim:current_dim+v_dim].astype(np.float32)
+            current_dim += v_dim
+        return output
+
+    def _return_space_variables_to_point_list(self, point_dic):
+        """Concatenates sample points from a dict back to the form 
+        np.array(number_of_points, self.dim)
+
+        Parameters
+        ----------
+        point_dic: dic
+            The dictionary of points 
+            (most likely created with divide_points_to_space_variables)
+        
+        Returns
+        -------
+        points: array
+            the point array of the form np.array(number_of_points, self.dim)
+        """
+        # if the points are not a dictonary just return
+        # (not created with our sampling)
+        if isinstance(point_dic, (list, np.ndarray)):
+            return point_dic
+        point_list = list(point_dic.values())
+        return np.column_stack(point_list)
 
     def _cut_points(self, n, points):
         """Deletes some random points, if more than n were sampled
@@ -177,25 +171,10 @@ class Domain:
             return points[index]
         return points
 
-    def _check_grid_enough_points(self, n, points):
-        """Checks if there are not enough points for the grid.
-        If not, add some random points.
-        """
-        if len(points) < n:
-            new_points = self.sample_random_uniform(n-len(points))
-            new_points = self.return_space_variables_to_point_list(new_points)
-            points = np.append(points, new_points, axis=0)
-        return points
-
-    def _check_single_point(self, points):
-        if len(np.shape(points)) <= 1:
-            points = np.array([points])
-        return points
-
 
 class BoundaryDomain(Domain):
     def __init__(self, domain):
-        super().__init__(domain.space, dim=domain.dim-1, tol=domain.tol)
+        super().__init__(domain.space, dim=domain.dim-1)
 
     @abc.abstractmethod
     def normal(self, points):
@@ -226,18 +205,17 @@ class ProductDomain(Domain):
                 of its factor domains spaces. This may lead to unexpected behaviour.""")
         space = self.domain_a.space * self.domain_b.space
         super().__init__(space=space,
-                         dim=domain_a.dim + domain_b.dim,
-                         tol=min(domain_a.tol, domain_b.tol))
+                         dim=domain_a.dim + domain_b.dim)
 
-    def project_on_subspace(self, subspace=None, **variables):
-        assert subspace in self.space
-        for domain in (self.domain_a, self.domain_b):
-            if subspace in domain.space:
-                return domain.project_on_subspace(subspace)
-        return ProductDomain(self.domain_a.project_on_subspace(subspace & self.domain_a.space),
-                             self.domain_b.project_on_subspace(subspace & self.domain_b.space))
-        # TODO: fix this to keep order of dimensions with identical names
+    @property
+    def boundary(self):
+        # Domain object of the boundary
+        return
 
+    @property
+    def inner(self):
+        # open domain
+        return ProductDomain(self.domain_a.inner, self.domain_b.inner)
 
     @abc.abstractmethod
     def __add__(self, other):
@@ -275,28 +253,14 @@ class ProductDomain(Domain):
         """
         return ProductDomain(self.domain_a & other, self.domain_b & other)
 
-    @abc.abstractmethod
-    def is_inside(self, points):
-        return 
+    def __contains__(self, points):
+        return
 
-    @abc.abstractmethod
     def bounding_box(self):
-        raise NotImplementedError
+        return
 
-    @abc.abstractmethod
     def sample_grid(self, n):
-        raise NotImplementedError
+        return
 
-    @abc.abstractmethod
     def sample_random_uniform(self, n):
-        raise NotImplementedError
-
-    @property
-    def boundary(self):
-        # Domain object of the boundary
-        return ProductDomain(self.domain_a.boundary, self.domain_b.boundary)
-
-    @property
-    def inner(self):
-        # open domain
-        return ProductDomain(self.domain_a.inner, self.domain_b.inner)
+        return
