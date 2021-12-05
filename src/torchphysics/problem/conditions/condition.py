400c9882daab8ade04732dfa8b604fb7fcdbd6d7
nick7 <nick7@uni-bremen.de>
2021-12-05 14:39:11 +0100

    add data function option to conditions
    Signed-off-by: nick7 <nick7@uni-bremen.de>

--- src/torchphysics/problem/conditions/condition.py
+++ src/torchphysics/problem/conditions/condition.py
@@ -9,7 +9,7 @@ import numpy as np
 from ...models import Parameter
 from ...utils import UserFunction
 from ..spaces import Points
-
+from ..samplers import StaticSampler
 """
 TODO: check creation of tensors on correct devices, maybe additional flags
     are necessary
@@ -54,6 +54,17 @@ class Condition(torch.nn.Module):
         for var in points_coordinates:
             points_coordinates[var].requires_grad = True
         return points_coordinates, Points.from_coordinates(points_coordinates)
+    
+    def _setup_data_functions(self, data_functions, sampler):
+        for fun in data_functions:
+            data_functions[fun] = UserFunction(data_functions[fun])
+        if isinstance(sampler, StaticSampler):
+            # functions can be evaluated once
+            for fun in data_functions:
+                points = next(sampler)
+                data_functions[fun] = UserFunction(data_functions[fun](points))
+        return data_functions
+        
 
 
 class DataCondition(Condition):
@@ -103,9 +114,11 @@ class PINNCondition(Condition):
     residual_fn : callable
         A user-defined function that computes the residual from inputs and outputs
         of the model, e.g. by using utils.differentialoperators
-    norm : torch.nn.Module
-        A torch Module which computes the (scalar) norm of the given residual,
-        e.g. torch.nn.MSELoss().
+    norm : int
+        The p of the used Lp-norm
+    data_functions : dict
+        A dictionary of user-defined functions and their names (as keys). Can be
+        used e.g. for right sides in PDEs.
     parameter : Parameter
         A Parameter that can be used in the residual_fn and should be learned in
         parallel, e.g. based on data.
@@ -121,22 +134,29 @@ class PINNCondition(Condition):
         solving forward and inverse problems involving nonlinear partial differential
         equations", Journal of Computational Physics, vol. 378, pp. 686-707, 2019.
     """
-    def __init__(self, module, sampler, residual_fn, norm=2, parameter=Parameter.empty(),
-                 name='pinncondition', weight=1.0):
+    def __init__(self, module, sampler, residual_fn, norm=2, data_functions={},
+                 parameter=Parameter.empty(), name='pinncondition', weight=1.0):
         super().__init__(name=name, weight=weight, track_gradients=True)
         self.module = module
         self.parameter = parameter
         self.sampler = sampler
         self.residual_fn = UserFunction(residual_fn)
         self.norm = norm
+        self.data_functions = self._setup_data_functions(data_functions, sampler)
     
     def forward(self):
         x = next(self.sampler)
         x_coordinates, x = self._track_gradients(x)
+
+        data = {}
+        for fun in self.data_functions:
+            data[fun] = self.data_functions[fun](x_coordinates)
+
         y = self.module(x)  # y is in coords of output space
-        return torch.mean(self.residual_fn({**y.coordinates,
-                                            **x_coordinates,
-                                            **self.parameter.coordinates})**self.norm,
+        return torch.mean(torch.abs(self.residual_fn({**y.coordinates,
+                                                      **x_coordinates,
+                                                      **self.parameter.coordinates,
+                                                      **data}))**self.norm,
                           dim=0)**(1/self.norm)
 
 
@@ -157,6 +177,9 @@ class DeepRitzCondition(Condition):
     integrand : callable
         A user-defined function that computes the integrand of the weak formulation,
         e.g. by using utils.differentialoperators
+    data_functions : dict
+        A dictionary of user-defined functions and their names (as keys). Can be
+        used e.g. for right sides in PDEs.
     parameter : Parameter
         A Parameter that can be used in the integrand and should be leraned in parallel,
         e.g. based on data.
@@ -168,24 +191,34 @@ class DeepRitzCondition(Condition):
 
     Notes
     -----
+    ..  The implementation differs from the PINNCondition with p=1 only in avoiding the
+        computation of absolute values in the integral. The main difference of PINN and
+        DeepRitz is the definition of the integrand by the user.
     ..  [1] Weinan E and Bing Yu, "The Deep Ritz method: A deep learning-based numerical
         algorithm for solving variational problems", 2017
     """
-    def __init__(self, module, sampler, residual_fn, parameter=Parameter.empty(),
-                 name='deepritzcondition', weight=1.0):
+    def __init__(self, module, sampler, residual_fn, data_functions={},
+                 parameter=Parameter.empty(), name='deepritzcondition', weight=1.0):
         super().__init__(name=name, weight=weight, track_gradients=True)
         self.module = module
         self.parameter = parameter
         self.sampler = sampler
         self.residual_fn = UserFunction(residual_fn)
+        self.data_functions = self._setup_data_functions(data_functions, sampler)
     
     def forward(self):
         x = next(self.sampler)
         x_coordinates, x = self._track_gradients(x)
+
+        data = {}
+        for fun in self.data_functions:
+            data[fun] = self.data_functions[fun](x_coordinates)
+
         y = self.module(x)
         return torch.mean(self.residual_fn({**y.coordinates,
                                             **x_coordinates,
-                                            **self.parameter.coordinates}),
+                                            **self.parameter.coordinates,
+                                            **data}),
                           dim=0)
 
 
--- src/torchphysics/problem/samplers/__init__.py
+++ src/torchphysics/problem/samplers/__init__.py
@@ -1,7 +1,8 @@
 """Objects that sample points on given a domain"""
 
 from .sampler_base import (PointSampler, ProductSampler, 
-                           ConcatSampler, AppendSampler)
+                           ConcatSampler, AppendSampler,
+                           StaticSampler)
 from .random_samplers import RandomUniformSampler, GaussianSampler, LHSSampler
 from .grid_samplers import GridSampler, ExponentialIntervalSampler
 from .plot_samplers import PlotSampler
